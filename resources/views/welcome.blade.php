a21c2549880250861bdd90c1cb41fbc459163a9b
Right <sarah82529@gmail.com>
2021-10-22 15:35:50 +0800

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Laravel</title>
+    <title>Ethereum Demo</title>
+    <link rel="icon" href="/favicon.ico">
 </head>
 <body>
 <div id="app" class="md:container md:mx-auto">
